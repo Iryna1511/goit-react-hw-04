14cdd510c0938ceec5009eb758ad9e50de5d4f40
Iryna1511 <148438865+Iryna1511@users.noreply.github.com>
2024-05-21 14:45:47 +0300

    styles gallery

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,8 +1,14 @@
+import css from "./ImageCard.module.css";
+
 export default function ImageCard({ images }) {
   return (
     <>
       <a href={images.urls.regular}>
-        <img src={images.urls.small} alt={images.description} />
+        <img
+          className={css.img}
+          src={images.urls.small}
+          alt={images.description}
+        />
       </a>
     </>
   );
